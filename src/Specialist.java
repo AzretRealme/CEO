e8f9913430ba5c3228dc0fa17bd7f1d86a4a10ef
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2020-11-12 01:35:49 +0600

    update
    update

--- src/Specialist.java
+++ src/Specialist.java
@@ -1,58 +1,18 @@
 public class Specialist extends Employee{
-    private int levels;
-    private double salary;
-    private String name;
-    private int age;
-
+      private int levels;
     public Specialist() {
-
     }
-    public Specialist(int levels) {
-
-        this.levels = levels;
+    public Specialist(String name, int age, double salary) {
+        super(name, age, salary);
     }
-
-    @Override
-    public int getAge() {
-        return age;
-    }
-
-    @Override
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public int getLevels() {
-
         return levels;
     }
-
     public void setLevels(int levels) {
-
         this.levels = levels;
     }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public void setName(String name) {
-
-        this.name = name;
-    }
-
-    @Override
-    public double getSalary() {
-        return salary;
-    }
-
-    @Override
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
     public void workWithClients(){
-        System.out.println("Specialist " + " workWithClients ");
+        //super.Work();
+        System.out.print("Specialist awesome speak English" + " workWithClients ");
     }
 }
