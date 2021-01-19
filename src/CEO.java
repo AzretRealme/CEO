b45159d668731d549482628feb3ae78b0e9a0840
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2021-01-19 07:56:24 +0600

    updated
    updated

--- src/CEO.java
+++ src/CEO.java
@@ -1,19 +1,20 @@
 public class CEO extends Employee{
-    private boolean hasCompanyCar;
+    boolean hasCompanyCar;
 
-    public CEO() {
-    }
-    public CEO(String name, int age, double salary){
+    public CEO(String name, int age, double salary) {
         super(name, age, salary);
     }
+
     public boolean isHasCompanyCar() {
         return hasCompanyCar;
     }
+
     public void setHasCompanyCar(boolean hasCompanyCar) {
         this.hasCompanyCar = hasCompanyCar;
     }
+
     public void goPublic(){
        // super.Work();
-        System.out.print("CEO - everyday work.  " + " -Work IPO.   ---");
+        System.out.print("CEO - everyday work.  " + " -Work BMW Company.   ---");
     }
 }
